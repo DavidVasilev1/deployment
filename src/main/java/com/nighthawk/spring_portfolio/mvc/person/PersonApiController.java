75a774bf01ab66a8f55e52501fba8b53836500e0
firestrom0986 <firestorm0986@gmail.com>
2024-01-22 21:37:02 -0800

    Delete with params

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.transaction.annotation.Transactional;
 
 
 
@@ -81,8 +81,8 @@ public class PersonApiController {
     
     // NEW CODE - ADI
     @Transactional
-    @DeleteMapping("/delete/{email}")
-    public ResponseEntity<Person> deletePerson(@PathVariable String email) {
+    @DeleteMapping("/delete")
+    public ResponseEntity<Person> deletePerson(@RequestParam("email") String email) {
         Person person = repository.findByEmail(email);
         
         if (person != null) {  // Check if the person is found
